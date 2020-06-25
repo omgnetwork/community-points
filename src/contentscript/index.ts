94a61d76a1056cab0719065757dc4ac841d02db4
nicholasmueller <nicholasmueller91@gmail.com>
2020-06-25 21:15:54 +0700

    2 way communication infra

--- src/contentscript/index.ts
+++ src/contentscript/index.ts
@@ -1,3 +1,5 @@
+import { get } from 'lodash';
+
 function injectWeb3Bridge() {
   const script = document.createElement('script');
   const bridge = chrome.extension.getURL('web3Bridge.js');
@@ -9,12 +11,26 @@ function injectWeb3Bridge() {
   node.appendChild(script);
 }
 
-// listen to ui messages and forward them to injected script
+// listen to ui messages and forward them to bridge
 chrome.runtime.onMessage.addListener(request => {
   window.postMessage({
-    key: 'web3Bridge',
+    key: 'to_web3Bridge',
     payload: request.payload
   }, '*');
+});
+
+// listen to bridge messages and forward them to ui
+window.addEventListener('message', async function (event) {
+  const key = get(event, 'data.key', null);
+  const payload = get(event, 'data.payload', null);
+  const caller = get(event, 'data.caller', null);
+  if (key !== 'from_web3Bridge') {
+    return;
+  }
+
+  if (caller === 'WEB3/ENABLE') {
+    console.log('payload received from web3: ', payload);
+  }
 });
 
 injectWeb3Bridge();
--- src/web3Bridge/index.ts
+++ src/web3Bridge/index.ts
@@ -1,14 +1,19 @@
 import { get } from 'lodash';
 
+// handle messages sent from ui in page context
 window.addEventListener('message', async function (event) {
   const key = get(event, 'data.key', null);
   const payload = get(event, 'data.payload', null);
-  if (key !== 'web3Bridge') {
+  if (key !== 'to_web3Bridge') {
     return;
   }
 
   if (payload === 'WEB3/ENABLE') {
     const result = await (window as any).ethereum.enable();
-    console.log('result: ', result);
+    window.postMessage({
+      key: 'from_web3Bridge',
+      caller: 'WEB3/ENABLE',
+      payload: result
+    }, '*');
   }
 });
