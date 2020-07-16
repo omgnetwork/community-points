c7aad8ac213d5a3da5026a96af08ca75b2c01712
Kevin Sullivan <4653170+kevsul@users.noreply.github.com>
2020-07-16 14:17:46 +0200

    check configured fee token is supported

--- fee-relayer/src/relay-tx.js
+++ fee-relayer/src/relay-tx.js
@@ -23,6 +23,9 @@ async function getFeeInfo (childChain, currency) {
   if (!feeInfo) {
     const fees = (await childChain.getFees())['1']
     feeInfo = fees.find(fee => fee.currency.toLowerCase() === currency.toLowerCase())
+    if (!feeInfo) {
+      throw new Error(`Configured FEE_TOKEN ${currency} is not a supported fee token`)
+    }
   }
   return feeInfo
 }
