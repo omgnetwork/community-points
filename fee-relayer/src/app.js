1ad0d48430199a4b5d5f9ef41dd563731af88005
nicholasmueller <nicholasmueller91@gmail.com>
2020-07-23 11:35:11 +0700

    typo

--- fee-relayer/src/app.js
+++ fee-relayer/src/app.js
@@ -30,7 +30,7 @@ const process = require('process')
 const Sentry = require('@sentry/node')
 
 if (process.env.SENTRY_DSN) {
-  Sentry.init({ dsn: config.sentryDsn });
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
   Sentry.configureScope(scope => {
     scope.setTag('layer', 'fee-relayer');
   });
