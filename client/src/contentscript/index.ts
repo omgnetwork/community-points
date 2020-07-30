842b9585a5a855d47eda95cd3f1b462abc08ae45
nicholasmueller <nicholasmueller91@gmail.com>
2020-07-30 22:10:41 +0700

    switch to pageaction for icon disabling

--- client/src/contentscript/index.ts
+++ client/src/contentscript/index.ts
@@ -6,34 +6,40 @@ import injectApp from 'contentscript/elements/injectApp';
 import injectBridge from 'contentscript/elements/injectBridge';
 
 // listen to ui messages and forward them to bridge
-chrome.runtime.onMessage.addListener((message: Partial<IMessage>) => {
-  if (message.type === 'BROWSERACTION/SHOW') {
-    return injectApp();
-  }
+chrome.runtime.onMessage.addListener(
+  function onMessageListener (message: Partial<IMessage>): void {
+    if (message.type === 'PAGEACTION/CLICKED') {
+      return injectApp();
+    }
 
-  return window.postMessage({
-    key: 'to_bridge',
-    type: message.type,
-    payload: message.payload
-  }, '*');
-});
+    return window.postMessage({
+      key: 'to_bridge',
+      type: message.type,
+      payload: message.payload
+    }, '*');
+  }
+);
 
 // listen to bridge messages and forward them to ui
-window.addEventListener('message', async function bridgeMessageEventListener (event) {
-  const key = get(event, 'data.key', null);
-  const type = get(event, 'data.type', null);
-  const status = get(event, 'data.status', null);
-  const payload = get(event, 'data.payload', null);
+window.addEventListener(
+  'message',
+  async function bridgeMessageEventListener (event): Promise<void> {
+    const key = get(event, 'data.key', null);
+    const type = get(event, 'data.type', null);
+    const status = get(event, 'data.status', null);
+    const payload = get(event, 'data.payload', null);
 
-  if (key !== 'from_bridge') {
-    return;
+    if (key !== 'from_bridge') {
+      return;
+    }
+
+    chrome.runtime.sendMessage({
+      type,
+      status,
+      payload
+    });
   }
-
-  chrome.runtime.sendMessage({
-    type,
-    status,
-    payload
-  });
-});
+);
 
 injectBridge();
+chrome.runtime.sendMessage({ type: 'PAGEACTION/SHOW' });
