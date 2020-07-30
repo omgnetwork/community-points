ee73be8d9ec23795a339307c4bd2f334905dff86
okalouti <omar@omisego.co>
2020-07-30 09:50:24 +0700

    change token symbol name to KARMA

--- demo/contracts/KarmaPoint.sol
+++ demo/contracts/KarmaPoint.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @notice Karma Point ERC20 implementation contract
  */
 contract KarmaPoint is ERC20 {
-    constructor() public ERC20("Karma Token", "KRP") {}
+    constructor() public ERC20("Karma Token", "KARMA") {}
 
     function mint(address account, uint256 amount) public {
         ERC20._mint(account, amount);
