734038e0afaa55d7a4fd98175240d63d9f3df154
nicholasmueller <nicholasmueller91@gmail.com>
2020-08-17 23:02:57 +0700

    use json instead

--- client/src/app/services/configService.ts
+++ client/src/app/services/configService.ts
@@ -4,9 +4,11 @@ import { IConfig } from 'interfaces';
 
 export async function fetchConfig (): Promise<IConfig> {
   const rawSubRedditMap = await transportService.get({
-    url: 'https://raw.githubusercontent.com/omgnetwork/community-points/nm-dynamic-flairs/client/subreddit.config.js'
+    url: 'https://raw.githubusercontent.com/omgnetwork/community-points/nm-dynamic-flairs/client/subreddit.config.json'
   });
+  const config = JSON.parse(rawSubRedditMap);
+  console.log('raw: ', rawSubRedditMap);
+  console.log('parsed: ', config);
 
-  console.log(rawSubRedditMap);
   return rawSubRedditMap;
 }
