47cd82a5b1feba76ea249496b3d0a44e71481c42
nicholasmueller <nicholasmueller91@gmail.com>
2020-08-18 12:10:53 +0700

    move flair config to root

--- client/src/app/services/configService.ts
+++ client/src/app/services/configService.ts
@@ -4,7 +4,7 @@ import { IConfig } from 'interfaces';
 
 export async function fetchConfig (): Promise<IConfig> {
   const config = await transportService.get({
-    url: 'https://raw.githubusercontent.com/omgnetwork/community-points/nm-dynamic-flairs/client/subreddit.config.json'
+    url: 'https://raw.githubusercontent.com/omgnetwork/community-points/nm-dynamic-flairs/subreddit.config.json'
   });
   return config;
 }
