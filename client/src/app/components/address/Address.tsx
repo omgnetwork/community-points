2a8bfb722a2388505cf80e2fe5397ec85d10abf1
nicholasmueller <nicholasmueller91@gmail.com>
2020-09-09 16:17:23 +0700

    render highest purchased in address

--- client/src/app/components/address/Address.tsx
+++ client/src/app/components/address/Address.tsx
@@ -29,11 +29,53 @@ function Address ({
   const purchasedFlairs: IFlairMap = useSelector(selectPurchasedFlairs);
   const session: ISession = useSelector(selectSession);
 
+  const unleveledFlairs = Object.values(session.subReddit.flairMap).filter(i => !i.metaId.includes(':'));
+
   useEffect(() => {
     if (username && !avatar) {
       dispatch(getUserAvatar(username));
     }
   }, [ username, avatar ]);
+
+  function renderPurchasedFlair (flairData: IFlair): JSX.Element {
+    return (
+      <img
+        key={flairData.metaId}
+        className={styles.flair}
+        src={flairData.icon}
+        alt={flairData.metaId}
+      />
+    );
+  }
+
+  function renderHighestFlairPurchased (metaId: string): JSX.Element {
+    const flairData: IFlair = Object.values(session.subReddit.flairMap).find(i => i.metaId === metaId);
+    if (!flairData) {
+      return null;
+    }
+
+    const purchased = purchasedFlairs[flairData.metaId];
+    if (purchased) {
+      // check if the next level up is available first
+      const splitMetaId = metaId.split(':');
+      const nextLevelMetaId = splitMetaId.length === 2
+        ? `${splitMetaId[0]}:${Number(splitMetaId[1]) + 1}`
+        : `${metaId}:2`;
+
+      const validNextLevel = Object.values(session.subReddit.flairMap).find(i => i.metaId === nextLevelMetaId);
+      if (!validNextLevel) {
+        return renderPurchasedFlair(flairData);
+      }
+
+      // check if next level purchased, if not render this one
+      const nextLevelPurchased = purchasedFlairs[nextLevelMetaId];
+      if (nextLevelPurchased) {
+        return renderHighestFlairPurchased(nextLevelMetaId);
+      }
+
+      return renderPurchasedFlair(flairData);
+    }
+  }
 
   return (
     <div
@@ -49,18 +91,8 @@ function Address ({
         clickable={false}
       />
       <div className={styles.purchasedFlairs}>
-        {Object.values(session.subReddit.flairMap).map((_flair: IFlair, index: number) => {
-          const purchased = purchasedFlairs[_flair.metaId];
-          if (purchased) {
-            return (
-              <img
-                key={index}
-                className={styles.flair}
-                src={_flair.icon}
-                alt='flair-icon'
-              />
-            );
-          }
+        {unleveledFlairs.map((unleveledFlair: IFlair) => {
+          return renderHighestFlairPurchased(unleveledFlair.metaId);
         })}
       </div>
     </div>
