27a8492320870be5b822cde6e6e568967655400e
nicholasmueller <nicholasmueller91@gmail.com>
2020-09-15 10:55:40 +0700

    flair upgrade test

--- client/src/app/services/configService.ts
+++ client/src/app/services/configService.ts
@@ -2,9 +2,12 @@ import * as transportService from 'app/services/transportService';
 
 import { IConfig } from 'interfaces';
 
+// const configBranch = 'master';
+const configBranch = 'nm-21-leaderboard-link';
+
 export async function fetchConfig (): Promise<IConfig> {
   const config = await transportService.get({
-    url: 'https://raw.githubusercontent.com/omgnetwork/community-points/master/client/subreddit.config.json'
+    url: `https://raw.githubusercontent.com/omgnetwork/community-points/${configBranch}/client/subreddit.config.json`
   });
   return config;
 }
